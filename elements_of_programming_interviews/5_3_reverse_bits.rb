a7f6abaedf2af3332f2908ff7c20610e146256fb
ShawnAukstak <Shawn.Aukstakalnis@gmail.com>
2017-01-27 17:03:30 -0500

    reverse bits

--- elements_of_programming_interviews/5_3_reverse_bits.rb
+++ elements_of_programming_interviews/5_3_reverse_bits.rb
@@ -3,11 +3,11 @@
 #110
 #011
 
-def reverse_bits(word)
+def reverse_bits_brute(word)
   left, right = 0, word.bit_length - 1
 
   while left < right
-    swap_bits(left, right, word)
+    word = swap_bits(left, right, word)
     left += 1
     right -= 1
   end
@@ -16,8 +16,18 @@ def reverse_bits(word)
 end
 
 def swap_bits(a, b, word)
-  
-  word[left], word[right] = word[right], word[left]
+  #Only swap if they are not equal
+  if (word >> a) & 1 != (word >> b) & 1
+
+    #They differ - only need to swap them
+    word ^= ((1 << a) | (1 << b))
+  end
+  word
 end
 
-puts reverse_bits(6)
+x = 6
+puts x.to_s(2)
+puts reverse_bits_brute(x).to_s(2)
+
+#This can be done much more efficiently O(1) runtime if we created a hash or store,
+#Such that that key is a 16 bit word, and the value is the 16 bit word reversed.
