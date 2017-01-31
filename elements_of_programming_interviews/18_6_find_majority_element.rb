7fd371e23d1e8114048fd3811ded90429f0a6ebf
ShawnAukstak <Shawn.Aukstakalnis@gmail.com>
2017-01-31 14:31:33 -0500

    wip

---
+++ elements_of_programming_interviews/18_6_find_majority_element.rb
@@ -0,0 +1,27 @@
+
+#Given a stream of words, more than half the words are a majority word.
+#The majority words could be anywhere in the scan
+#Scan the stream once, using only constant memory to determine what the majority word
+#O(n) O(1)
+
+def majority_element(stream)
+  candidate, count = "", 0
+
+  stream.each(" ") do |w|
+    if count == 0
+      candidate = w
+      count = 1
+    elsif candidate == w
+      count += 1
+    else
+      count -= 1
+    end
+  end
+
+  candidate
+end
+
+string = "meow meow woof meow bang woof meow woof meow"
+
+stream = StringIO.new(string)
+puts majority_element(stream)
